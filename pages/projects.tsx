260289fb5f7fe4d750661a00cac5d1c2664f97b4
msdio <rlagofka1@gmail.com>
2023-01-14 23:32:02 +0900

    getting datas from notion

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Layout from "../components/layout";
+import { DATABASE_ID, TOKEN } from "../config/index";
 
 export default function Projects() {
     return (
@@ -16,4 +17,31 @@ export default function Projects() {
             <h1>Project</h1>
         </Layout>
     );
+}
+
+export async function getStaticProps() {
+    const options = {
+        method: "POST",
+        headers: {
+            accept: "application/json",
+            "Notion-Version": "2022-02-22",
+            "content-type": "application/json",
+            Authorization: `Bearer ${TOKEN}`,
+        },
+        body: JSON.stringify({ page_size: 100 }),
+    };
+
+    const res = await fetch(
+        `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+        options
+    );
+    const projects = await res.json();
+
+    const projectIds = projects.results.map((item: any) => item.id);
+
+    console.log(projects);
+
+    return {
+        props: {}, // will be passed to the page component as props
+    };
 }
